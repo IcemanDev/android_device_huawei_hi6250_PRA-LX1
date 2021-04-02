bc1c7b5acf8414aa8e25cc9047d3667cf156a9a6
IceMan <cammaratamichele1966@gmail.com>
2021-04-02 22:00:40 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,20 +95,3 @@ TW_HAS_EDL_MODE := false
 
 # Set language default
 TW_DEFAULT_LANGUAGE := en
-
-# For Version TWRP
-#TW_SPECIFIC_VERSION_STR := ""
-TW_DEVICE_VERSION := "PRA LX1 by IceMan"
-
-# For time custom
-TW_SPECIFIC_TIME_ZONE_VAR := "CET-1CEST,M3.5.0,M10.5.0"
-TW_SPECIFIC_TIME_ZONE_GUISEL := "CET-1;CEST,M3.5.0,M10.5.0"
-
-# For H24 hours (for H12 hours "0")
-TW_SPECIFIC_MILITARY_TIME := "1"
-
-# For Screen_timeout_secs
-TW_SCREEN_TIMEOUT_SECS := "120"
-
-#For reverse navbar (default "0")
-TW_SPECIFIC_SAMSUNG_NAVBAR := "0"
